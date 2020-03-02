63e8d4639cb6a070594cb3eed6b3535c96276890
Philipp Hamm <philipp.hamm@gmail.com>
2020-03-02 16:53:48 +0100

    Database and model setup

---
+++ app/models/activity.rb
@@ -0,0 +1,12 @@
+class Activity < ApplicationRecord
+  belongs_to :user
+  validates :datetime, presence: true
+  # geocoded_by :address
+  # has_many photos for cloudinary ...
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category, presence: true
+  validates :capacity, presence: true
+  validates :datetime, presence: {message: "Date cannot be blank."}
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :activites
+  has_many :bookings
+  validates :email, presence: true, format: { with: /\A.*@.*\.com\z/ }, uniqueness: true
 end
