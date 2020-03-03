d2acdc2683e8d7b78b02387bbcafaadbca08bef0
Philipp Hamm <philipp.hamm@gmail.com>
2020-03-03 16:34:43 +0100

    Work in progress

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations" }
   root to: 'pages#home'
   get 'dashboard', to: 'pages#dashboard', as: :dashboard
+
   resources :activities do
     resources :bookings, only: [:create, :new]
   end
