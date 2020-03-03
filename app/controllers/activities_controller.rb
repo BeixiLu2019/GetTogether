cd30232daffb04cd16475567b75e97432901b298
Ambar Galan <ambar.galan@gmail.com>
2020-03-03 17:48:10 +0100

    finished Pundit and fixes around

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -3,52 +3,46 @@ skip_before_action :authenticate_user!, only: [:index, :show]
 before_action :set_activity, only: [:show, :edit, :update, :destroy]
 
   def index
-    @activities = Activity.all
-  end
-
-  def show
-    # authorize @office
-    @activity = Activity.find(params[:id])
-    # authorize @booking
-
+    @activities = policy_scope(Activity)
+    # @activities = Activity.all
   end
 
   def new
     @user = current_user
     @activity = Activity.new
-    # authorize @activity
+    authorize @activity
+  end
+
+  def show
+    # authorize @booking
   end
 
   def create
     @activity = Activity.new(activity_params)
     @activity.user = current_user
-    # authorize @office
+    authorize @activity
     @activity.save
     redirect_to activity_path(@activity)
   end
 
   def edit
-    #authorize @activity
   end
 
   def update
-    #authorize @activity
     @activity.update(activity_params)
     redirect_to activity_path(@activity)
   end
 
   def destroy
-    #authorize @activity
     @activity.destroy
-    redirect_to activities_path # to be updated
+    redirect_to activities_path
   end
 
 private
 
   def set_activity
     @activity = Activity.find(params[:id])
-    # for when we install Pundit :
-    # authorize @office
+    authorize @activity
   end
 
   def activity_params
