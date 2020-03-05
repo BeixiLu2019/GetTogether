9fa8e42e79f21f10486d4a967e04749a43b67900
Beixi <56199524+BeixiLu2019@users.noreply.github.com>
2020-03-05 15:44:04 +0100

    Update registrations_controller.rb

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,19 +1,6 @@
 class RegistrationsController < Devise::RegistrationsController
   before_action :set_user, only: [:edit, :update]
-  # def after_sign_up_path_for(resource)
-  #   edit_user_registration_path
-  # end
-
-  # def edit
-  #    # authorize @user
-  # end
-
-  # def update
-  #   authorize @user
-  #   @user.update(user_params)
-  #   redirect_to user_path(current_user)
-  # end
-
+  
   private
 
   def set_user
