3da854d84e8a37a25671290e90c4dff3180725b6
Beixi <56199524+BeixiLu2019@users.noreply.github.com>
2020-03-10 10:33:18 +0100

    Revert "Conflict merge"

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,34 +9,8 @@ import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the styleshe
 import { initMapbox } from '../plugins/init_mapbox';
 initMapbox();
 
-// sweet alert import:
-import { initSweetalert } from '../plugins/init_sweetalert';
-
-initSweetalert('#sweet-alert-demo', {
-  title: "Confirmed",
-  // closeOnClickOutside: false,
-  button: false,
-  timer: 3000,
-  text: "You are going! Don't be late 🕙",
-  icon: "success"
-}, (value) => {
-  console.log('yaya!!', value)
-  const link = document.querySelector('#confirm');
-  link.click();
-});
-
-initSweetalert('#sweet-alert-cancel', {
-  text: "Are you sure you want to cancel this booking?",
-  icon: "warning",
-  dangerMode: true,
-  buttons: true,
-}, (value) => {
-  if (value) {
-    const link = document.querySelector('#delete');
-    link.click();
-  }
-});
-
+import { initTimer } from '../plugins/init_timer';
+initTimer();
 
 
 
