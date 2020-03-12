2c29df1d1e59d26fa2875f6cf0669b87bc93fc6f
Philipp Hamm <philipp.hamm@gmail.com>
2020-03-12 09:16:09 +0100

    Yes

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -1,9 +1,18 @@
 function scrollLastMessageIntoView() {
-  console.log("Awesome")
   var messages = document.querySelectorAll('.chat-message');
-  var lastMessage = messages[messages.length - 1];
+  var lastMessage = messages[messages.length-1];
 
   if (lastMessage !== undefined) {
     lastMessage.scrollIntoView();
+  }
+}
+
+function scrollFirstMessageIntoView() {
+  var messages = document.querySelectorAll('.chat-message');
+  var firstMessage = messages.first;
+  console.log(firstMessage);
+
+  if (firstMessage !== undefined) {
+    firstMessage.scrollIntoView();
   }
 }
