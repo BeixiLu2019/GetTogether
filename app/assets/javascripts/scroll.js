df699211b4b917f23a4a38b2d632100d3b1c3e1d
Philipp Hamm <philipp.hamm@gmail.com>
2020-03-12 11:47:44 +0100

    hello

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -1,5 +1,9 @@
+
+
 function scrollLastMessageIntoView() {
+  console.log('what')
   var messages = document.querySelectorAll('.chat-message');
+  console.log(messages);
   var lastMessage = messages[messages.length-1];
 
   if (lastMessage !== undefined) {
@@ -9,10 +13,18 @@ function scrollLastMessageIntoView() {
 
 function scrollFirstMessageIntoView() {
   var messages = document.querySelectorAll('.chat-message');
-  var firstMessage = messages.first;
-  console.log(firstMessage);
+  var firstMessage = messages[0];
 
   if (firstMessage !== undefined) {
-    firstMessage.scrollIntoView();
+    firstMessage.scrollIntoView(true);
+  }
+}
+
+function scrolling() {
+  var messages = document.querySelectorAll('.chat-message');
+  if (messages.length > 2) {
+    scrollLastMessageIntoView();
+  } else {
+    scrollFirstMessageIntoView();
   }
 }
